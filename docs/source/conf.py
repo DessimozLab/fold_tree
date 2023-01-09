9ef37666024902e0d1096a0c4164d27f07759060
cactuskid <dmoi@unil.ch>
2023-01-09 16:15:19 +0100

    autodoc modif

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,1 +1,11 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../../'))
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+
+# List of modules to mock import when building documentation
+autodoc_mock_imports = ['treescore' , 'foldseek2tree' , 'AFDB_tools']
+
+# Default order for members
+autodoc_member_order = 'bysource'
