5b0bf93ed310d62b740911f27323ed34e5f30199
cactuskid <dmoi@unil.ch>
2023-01-10 11:55:18 +0100

    set theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,8 @@ import sys
 
 sys.path.insert(0, os.path.abspath('../../'))
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+html_theme = "classic"
+
 
 # List of modules to mock import when building documentation
 autodoc_mock_imports = ['treescore' , 'foldseek2tree' , 'AFDB_tools']
