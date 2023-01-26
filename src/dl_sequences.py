fa97ea75ec625b0d637c21a7439248d61f2128fd
cactuskid <dmoi@unil.ch>
2023-01-26 11:07:04 +0100

    reworking struct and seq queries

--- src/dl_sequences.py
+++ src/dl_sequences.py
@@ -5,9 +5,4 @@ with open(snakemake.input[0]) as infile:
 print(ids)
 
 resdf = AFDB_tools.grab_entries(ids, verbose = True)
-
-fasta = AFDB_tools.res2fasta(resdf)
-
-resdf.to_csv(snakemake.output[1])
-with open(snakemake.output[0] , 'w') as outfile:
-	outfile.write(fasta)
+resdf.to_csv(snakemake.output[0])
