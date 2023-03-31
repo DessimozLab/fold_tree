ac8213c9eaa987824c63948d4eb2f72a3790d2e3
cactuskid <dmoi@unil.ch>
2023-03-31 13:47:43 +0200

    fix conf

--- docsource/conf.py
+++ docsource/conf.py
@@ -6,14 +6,16 @@ sys.path.append(os.path.abspath('../src/'))
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode']
 html_theme = 'sphinx_rtd_theme'
 
-import AFDB_tools, foldseek2tree, treescore , corecut 
 
-
-# List of modules to mock import when building documentation
 
 autodoc_mock_imports = ['scipy' , 'numpy' , 'wget' , 'statsmodels' , 'toytree', 'pandas' , 're' , 'os' , 'scipy.stats' , 'argparse' , 'subprocess' , 'shlex' , 
 'scipy.spatial.distance' , 'scipy.stats' , 'statsmodels', 'Bio' , 'requests'
 ]
+
+import AFDB_tools, foldseek2tree, treescore , corecut 
+
+
+# List of modules to mock import when building documentation
 
 
 # Default order for members
