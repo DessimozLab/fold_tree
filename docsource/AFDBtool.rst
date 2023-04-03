8699745843fc1e36c89fe27a573aaebb2e7f164c
Mauricio Langleib <mauricio.langleib@gmail.com>
2023-04-03 18:39:56 -0300

    Revert "Merge branch 'docsource_update'"
    This reverts commit 9ef105845b6c805901b6e6499d1d7c8fb3d4a54a, reversing
    changes made to 649694c8ce2f3c94beeaf0dbd01f043031e49412.

--- docsource/AFDBtool.rst
+++ docsource/AFDBtool.rst
@@ -2,15 +2,7 @@
 AFDB tools
 =====================
 
-The AFDB tools module contains utility functions to download target AlphaFold structural models, perform actions on them and interact with the UniProt server.
-
-Briefly, the user can:
-
-- Download a set of target AlphaFold structural models using the `grab_struct` function.
-- Get descriptive statistics for an AlphaFold model's pLDDT metrics (minimum and maximum value, mean, variance, etc) using the `descr` function.
-- Filter AlphaFold structural models based on their residue's pLDDT metric using the `filter_plddt` function, discarding models presenting mean and/or minimum pLDDT values below a specified score.
-- Request protein information from the UniProt server for a given protein name or set of proteins using the `unirequest` and `grab_entries` functions. 
-- Convert data frames obtained from the UniProt server into FASTA format using the `res2fasta` function.
+This module contains the utility functions for alpha fold db and uniprot.
 
 .. automodule:: src.AFDB_tools
     :members:
--- src/AFDB_tools.py
+++ src/AFDB_tools.py
@@ -12,7 +12,6 @@ import numpy as np
 def descr(pdb_path):
 	'''
 	Extracts the plddt (in the beta factor column) of the first atom of each residue in a PDB file and returns a descriptive statistics object.
-
 	Parameters:
 		pdb_path (str): The path to the PDB file.'''
 
@@ -52,19 +51,19 @@ def grab_struct(uniID, structfolder, rejected = None, overwrite=False):
 	Downloads a protein structure file from the AlphaFold website and saves it to the specified folder.
 	
 	Parameters:
-		uniID (str): The UniProt ID of the protein for which the structure is being downloaded.
-		structfolder (str): The path to the folder where the structure file should be saved.
-		overwrite (bool, optional): A flag indicating whether to overwrite an existing file with the same name in the specified folder. Defaults to False.
+	uniID (str): The UniProt ID of the protein for which the structure is being downloaded.
+	structfolder (str): The path to the folder where the structure file should be saved.
+	overwrite (bool, optional): A flag indicating whether to overwrite an existing file with the same name in the specified folder. Defaults to False.
 	
 	Returns:
-		None: If the file is successfully downloaded or if overwrite is set to True and a file with the same name is found in the specified folder.
-		str: If an error occurs during the download or if a file with the same name is found in the specified folder and overwrite is set to False.
+	None: If the file is successfully downloaded or if overwrite is set to True and a file with the same name is found in the specified folder.
+	str: If an error occurs during the download or if a file with the same name is found in the specified folder and overwrite is set to False.
 	
 	Examples:
-		>>> grab_struct('P00533', '/path/to/structures/')
-		None
-		>>> grab_struct('P00533', '/path/to/structures/', overwrite=True)
-		None
+	>>> grab_struct('P00533', '/path/to/structures/')
+	None
+	>>> grab_struct('P00533', '/path/to/structures/', overwrite=True)
+	None
 	"""
 
 	try:
@@ -95,16 +94,16 @@ def unirequest_tab(name, verbose = False):
 	Makes a request to the UniProt API and returns information about a protein in tab-separated format.
 	
 	Parameters:
-		name (str): The name of the protein for which information is being requested.
-		verbose (bool, optional): A flag indicating whether to print the returned data to the console. Defaults to False.
+	name (str): The name of the protein for which information is being requested.
+	verbose (bool, optional): A flag indicating whether to print the returned data to the console. Defaults to False.
 	
 	Returns:
-		pd.DataFrame: A DataFrame containing information about the protein, with one row for each hit in the search.
+	pd.DataFrame: A DataFrame containing information about the protein, with one row for each hit in the search.
 	
 	Examples:
-		>>> unirequest_tab('P00533')
-																 id  ...                                            sequence
-		0  sp|P00533|1A2K_HUMAN RecName: Full=Alpha-2-...  ...  MPTSVLLLALLLAPAALVHVCRSRFPKCVVLVNVTGLFGN...
+	>>> unirequest_tab('P00533')
+															 id  ...                                            sequence
+	0  sp|P00533|1A2K_HUMAN RecName: Full=Alpha-2-...  ...  MPTSVLLLALLLAPAALVHVCRSRFPKCVVLVNVTGLFGN...
 	"""
 	#we query first by protein name and then gene name
 	url = 'http://rest.uniprot.org/uniprotkb/stream?'
@@ -133,20 +132,20 @@ def grab_entries(ids, verbose = True):
 	Makes requests to the UniProt API for information about proteins with the given IDs.
 	
 	Parameters:
-		ids (list): A list of UniProt IDs for the proteins for which information is being requested.
-		verbose (bool, optional): A flag indicating whether to print the returned data to the console. Defaults to False.
+	ids (list): A list of UniProt IDs for the proteins for which information is being requested.
+	verbose (bool, optional): A flag indicating whether to print the returned data to the console. Defaults to False.
 	
 	Returns:
-		pd.DataFrame: A DataFrame containing information about the proteins, with one row for each hit in the search.
+	pd.DataFrame: A DataFrame containing information about the proteins, with one row for each hit in the search.
 	
 	Examples:
-		>>> grab_entries(['P00533', 'P15056'])
-																 id  ...                                            sequence
-		0  sp|P00533|1A2K_HUMAN RecName: Full=Alpha-2-...  ...  MPTSVLLLALLLAPAALVHVCRSRFPKCVVLVNVTGLFGN...
-		1  sp|P15056|1A01_HUMAN RecName: Full=Alpha-1-...  ...  MAAARLLPLLPLLLALALALTETSCPPASQGQRASVGDRV...
+	>>> grab_entries(['P00533', 'P15056'])
+															 id  ...                                            sequence
+	0  sp|P00533|1A2K_HUMAN RecName: Full=Alpha-2-...  ...  MPTSVLLLALLLAPAALVHVCRSRFPKCVVLVNVTGLFGN...
+	1  sp|P15056|1A01_HUMAN RecName: Full=Alpha-1-...  ...  MAAARLLPLLPLLLALALALTETSCPPASQGQRASVGDRV...
 	
 	Notes:
-		This function makes requests to the UniProt API for information about proteins with the given IDs. If a request is successful, the returned data is processed and added to a DataFrame. If a request is unsuccessful, an error message is printed to the console.
+	This function makes requests to the UniProt API for information about proteins with the given IDs. If a request is successful, the returned data is processed and added to a DataFrame. If a request is unsuccessful, an error message is printed to the console.
 	"""
 	name_results = pd.concat([unirequest_tab( '+OR+'.join(c) , verbose = True) for c in chunk(ids, 50 )] , ignore_index= True)
 	if verbose == True:
@@ -158,15 +157,15 @@ def res2fasta(unires_df):
 	Converts a DataFrame containing protein information into a FASTA format string.
 	
 	Parameters:
-		unires_df (pd.DataFrame): A DataFrame containing information about proteins, with columns 'query' and 'Sequence' representing the name and sequence of each protein, respectively.
+	unires_df (pd.DataFrame): A DataFrame containing information about proteins, with columns 'query' and 'Sequence' representing the name and sequence of each protein, respectively.
 	
 	Returns:
-		str: A string in FASTA format representing the proteins in the input DataFrame.
+	str: A string in FASTA format representing the proteins in the input DataFrame.
 	
 	Examples:
-		>>> unires_df = pd.DataFrame([{'query': 'P00533', 'Sequence': 'MPTSVLLLALLLAPAALVHVCRSRFPKCVVLVNVTGLFGN'}])
-		>>> res2fasta(unires_df)
-		'> P00533\nMPTSVLLLALLLAPAALVHVCRSRFPKCVVLVNVTGLFGN\n'
+	>>> unires_df = pd.DataFrame([{'query': 'P00533', 'Sequence': 'MPTSVLLLALLLAPAALVHVCRSRFPKCVVLVNVTGLFGN'}])
+	>>> res2fasta(unires_df)
+	'> P00533\nMPTSVLLLALLLAPAALVHVCRSRFPKCVVLVNVTGLFGN\n'
 	"""
 	
 	unires_df = unires_df.drop_duplicates(subset=['query'])
