656b295063929228f81aad990f631cd496ef2153
cactuskid <cactuskid13@gmail.com>
2024-02-15 18:14:43 +0100

    job scripts

---
+++ all_hog_levels_ML.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+program="snakemake -s ./workflow/ML3ditree --profile slurmsimple/simple/ --config filter=True iqtree_cores=4 -k --rerun-incomplete --use-conda --directory  "
+folders=($(ls -d /work/FAC/FBM/DBC/cdessim2/default/dmoi/projects/snake_tree/OMA_data/* ) )
+for folder in "${folders[@]}"; do
+  echo $program "$folder"
+  $program "$folder" --unlock
+  $program "$folder" 
+done
+
+
