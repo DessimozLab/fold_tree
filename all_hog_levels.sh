17e379c146084c9220078098c6ee41f69293268f
catuskid <cactuskid13@gmail.com>
2024-04-10 14:49:18 +0200

    bash script param

--- all_hog_levels.sh
+++ all_hog_levels.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-program="snakemake --profile slurmsimple/simple/ --config filter=True -k --rerun-incomplete --use-conda --directory  "
-unlock="snakemake --profile slurmsimple/simple/ --config filter=True -k --rerun-incomplete --use-conda --unlock --directory  "
+program="snakemake --profile slurmsimple/simple/ --config filter=True -k --rerun-incomplete --use-conda --touch --directory  "
+unlock="snakemake --profile slurmsimple/simple/ --config filter=True -k --rerun-incomplete  --use-conda --unlock --directory  "
 
 folders=($(ls -d /work/FAC/FBM/DBC/cdessim2/default/dmoi/projects/snake_tree/OMA_data/* ) )
 for folder in "${folders[@]}"; do
