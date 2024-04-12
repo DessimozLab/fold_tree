ae89a65e53a26f6c2cc9f45ca8752334e1a88ba1
catuskid <cactuskid13@gmail.com>
2024-04-12 14:57:51 +0200

    update config subsample

--- src/dl_sequences.py
+++ src/dl_sequences.py
@@ -36,6 +36,5 @@ if custom_structs == True and snakemake.params.cath == False:
 else:
 	with open(snakemake.input[0]) as infile:
 		ids = [ i.strip() for i in infile if len(i.strip())>0 ]
-
 	resdf = AFDB_tools.grab_entries(ids, verbose = True)
 	resdf.to_csv(snakemake.output[0])
