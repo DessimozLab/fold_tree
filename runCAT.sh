458eee00306045cb0ea6758d3903a9a7ee564563
catuskid <cactuskid13@gmail.com>
2024-04-14 13:19:48 +0200

    update x aa pipeline

--- runCAT.sh
+++ runCAT.sh
@@ -1,2 +1,2 @@
 
-snakemake -s ./workflow/benchmark_all --profile slurmsimple/simple/ --config custom_structs=True cath=True filter=False iqtree_cores=1 iqtree_redo=True -k --touch --rerun-incomplete --use-conda --directory ./CAT_data
+snakemake -s ./workflow/benchmark_all --profile slurmsimple/simple/ --config custom_structs=True cath=True filter=False iqtree_cores=1 iqtree_redo=False -k --rerun-incomplete --use-conda --directory ./CAT_data
