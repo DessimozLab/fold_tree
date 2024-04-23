69733ca06ae2ee15098ed9916ca7b88e9c58825e
cactuskid <cactuskid13@gmail.com>
2024-04-23 09:14:25 +0200

    notebook metrics

--- all_hog_levels_BM_unfiltered.sh
+++ all_hog_levels_BM_unfiltered.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-program="snakemake -s ./workflow/benchmark_all --profile slurmsimple/simple/ -T 5 --config filter=False clean=False iqtree_redo=False customstructs=False  -k --rerun-incomplete --use-conda --directory   "
+program="snakemake -s ./workflow/benchmark_all --profile slurmsimple/simple/ -T 5 --config filter=False clean=False iqtree_redo=False fam_limit=500 customstructs=False  -k --rerun-incomplete --use-conda --directory   "
 folders=($(ls -d /work/FAC/FBM/DBC/cdessim2/default/dmoi/projects/snake_tree/OMA_data_unfiltered/OMA_data/* ) )
 
 for folder in "${folders[@]}"; do
