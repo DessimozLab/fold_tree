3b7888df88f28e440c794d39bd76df5afb3b8a31
catuskid <cactuskid13@gmail.com>
2024-04-27 11:16:30 +0200

    fix err

--- src/treescore.py
+++ src/treescore.py
@@ -153,7 +153,7 @@ def node_degree_inv(node , max_degree = 0):
 		if node.lineage:
 			node.add_feature( 'inv_degree' ,  len(node.lineage)* ( max_degree - node.degree) / max_degree )
 		else:
-			node.add_feature( 'inv_degree' ,  len(node.lineage)* ( max_degree - node.degree) / max_degree )
+			node.add_feature( 'inv_degree' ,  0 )
 		for i,c in enumerate(node.get_children()):
 			node_degree_inv(c , max_degree= max_degree)
 	return sum([n.inv_degree for n in node.traverse() if n.is_leaf() == False ])
