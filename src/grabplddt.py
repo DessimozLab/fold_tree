62b9fd2d968a7e648c0889ad7706d3e3f685287f
cactuskid <cactuskid13@gmail.com>
2025-09-24 13:42:02 +0200

    fix mamba err mk4

--- src/grabplddt.py
+++ src/grabplddt.py
@@ -7,7 +7,7 @@ all_structs ={}
 for struct in glob.glob(path):
     all_structs[struct] = AFDB_tools.descr(struct)
     #unpack scipy stats describe object into a dictionary
-    all_structs[struct] = { k:v for k,v in zip( ['nobs', 'minmax', 'mean', 'variance', 'skewness', 'kurtosis'], [ float(x) for x in list(all_structs[struct])]  ) }
+    all_structs[struct] = { k:v for k,v in zip( ['nobs', 'minmax', 'mean', 'variance', 'skewness', 'kurtosis'], [ float(x) if type(x) is not tuple else (float(x[0]),float(x[1])) for x in list(all_structs[struct])]  ) }
 #write to log file
 with open(snakemake.log[0], 'w' ) as lfout:
     lfout.write(str(all_structs))
